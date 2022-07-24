a6e6564de0062a8aaa2b425d6d1359115831bb45
Francisco Alvidrez <falvid16@gmail.com>
2022-07-24 12:55:41 -0600

    Initial project commit ready for deployment

---
+++ pokemon_api/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y0h#i=v(d9-obp&2))2zy+&_g!letvx)ufmcevb8b+8nay&a@b'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ pokemon_api/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['https://drf-pokemon.herokuapp.com/']
+
+CSRF_TRUSTED_ORIGINS = ['https://drf-pokemon.herokuapp.com/']
