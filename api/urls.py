bb4fb3d0643bc96833e0961306395a88fe472ce6
Francisco Alvidrez <falvid16@gmail.com>
2022-07-24 18:31:08 -0600

    Added the pokemon details url to url patterns

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import getData, addPokemon
+from .views import getData, addPokemon, pokemonDetails
 
 urlpatterns = [
     path('', getData),
     path('add-pokemon/', addPokemon),
+    path('pokemon/<int:pk>', pokemonDetails)
 ]
